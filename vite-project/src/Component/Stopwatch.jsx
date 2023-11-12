ab942467e4deff3c655c324e56f1c41f0332a969
Tinatajik <tinatajikrostamii@gmail.com>
2023-11-12 22:04:27 +0330

    handle-start-stop-reset

--- vite-project/src/Component/Stopwatch.jsx
+++ vite-project/src/Component/Stopwatch.jsx
@@ -2,15 +2,102 @@ import React, { useState, useEffect } from "react";
 import "./StopWatch.css";
 
 export default function StopWatch() {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [miliSeconds, setMiliSeconds] = useState(0);
+  const [laps, setLaps] = useState([]);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const formatWithLeadingZero = (number) => {
+    if (number < 10) return "0" + number;
+    else return number.toString();
+  };
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setMiliSeconds((miliSeconds) => {
+          if (miliSeconds >= 99) {
+            setSeconds((seconds) => {
+              if (seconds >= 59) {
+                setMinutes((minutes) => {
+                  if (minutes >= 59) {
+                    setHours((prevHours) => prevHours + 1);
+                    return 0;
+                  } else {
+                    return minutes + 1;
+                  }
+                });
+                return 0;
+              } else {
+                return seconds + 1;
+              }
+            });
+            return 0;
+          } else {
+            return miliSeconds + 1;
+          }
+        });
+      }, 10);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handlePause = () => {
+    setIsRunning(false);
+  };
+
+  const handleLap = () => {
+    const lapTime =
+      formatWithLeadingZero(hours) +
+      ":" +
+      formatWithLeadingZero(minutes) +
+      ":" +
+      formatWithLeadingZero(seconds) +
+      "." +
+      formatWithLeadingZero(miliSeconds);
+    setLaps((prevLaps) => [...prevLaps, lapTime]);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setMiliSeconds(0);
+    setSeconds(0);
+    setMinutes(0);
+    setHours(0);
+    setLaps([]);
+  };
+
   return (
     <div className="container">
-      <div className="timeDisplay"></div>
+      <div className="timeDisplay">
+        {formatWithLeadingZero(hours)} : {formatWithLeadingZero(minutes)} :{" "}
+        {formatWithLeadingZero(seconds)} : {formatWithLeadingZero(miliSeconds)}
+      </div>
       <div className="buttons">
-        <button className="btn bg-[#18a71e]">Start</button>
-
-        <button className="btn bg-[#187ca7]">Lap</button>
-        <button className="btn bg-[#a71818]">Stop</button>
-        <button className="btn bg-[#fbc31e]">Reset</button>
+        <button
+          className="btn bg-[#18a71e]"
+          onClick={handleStart}
+          disabled={isRunning}
+          style={{ cursor: isRunning ? "not-allowed" : "pointer" }}
+        >
+          Start
+        </button>
+        <button className="btn bg-[#187ca7]" onClick={handleLap}>
+          Lap
+        </button>
+        <button className="btn bg-[#a71818]" onClick={handlePause}>
+          Stop
+        </button>
+        <button className="btn bg-[#fbc31e]" onClick={handleReset}>
+          Reset
+        </button>
       </div>
     </div>
   );
